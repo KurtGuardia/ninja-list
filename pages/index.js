362b2dd2ad5a091e13a309578268682ef9f98758
Kurt Guardia <kaguardiagamarra@wideagency.com>
2022-09-22 12:39:19 +0200

    individual ninja

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Et odit beatae quae, aliquid esse aspernatur!
-        Adipisci, corrupti! Adipisci maxime, in consectetur
-        eveniet, voluptatibus laudantium suscipit quo, nulla
-        impedit at eius.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Et odit beatae quae, aliquid esse aspernatur!
-        Adipisci, corrupti! Adipisci maxime, in consectetur
-        eveniet, voluptatibus laudantium suscipit quo, nulla
-        impedit at eius.
-      </p>
-      <a className={styles.btn}>
-        {' '}
-        <Link href='/ninjas'>See Ninjas Listing</Link>{' '}
-      </a>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Et odit beatae quae, aliquid
+          esse aspernatur! Adipisci, corrupti! Adipisci
+          maxime, in consectetur eveniet, voluptatibus
+          laudantium suscipit quo, nulla impedit at eius.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Et odit beatae quae, aliquid
+          esse aspernatur! Adipisci, corrupti! Adipisci
+          maxime, in consectetur eveniet, voluptatibus
+          laudantium suscipit quo, nulla impedit at eius.
+        </p>
+        <a className={styles.btn}>
+          {' '}
+          <Link href='/ninjas'>
+            See Ninjas Listing
+          </Link>{' '}
+        </a>
+      </div>
+    </>
   )
 }
