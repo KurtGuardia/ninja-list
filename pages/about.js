fb6330db06e80f7538cf41fc62e31a9a3686fe8c
Kurt Guardia <kaguardiagamarra@wideagency.com>
2022-12-20 19:12:58 +0100

    basics

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Sint, assumenda voluptates ullam vitae
+        necessitatibus nulla. Repellendus a eum beatae
+        consequatur, ipsa saepe error deleniti, est expedita
+        quia exercitationem iusto. Vel?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Sint, assumenda voluptates ullam vitae
+        necessitatibus nulla. Repellendus a eum beatae
+        consequatur, ipsa saepe error deleniti, est expedita
+        quia exercitationem iusto. Vel?
+      </p>
+    </div>
+  )
+}
+
+export default About
